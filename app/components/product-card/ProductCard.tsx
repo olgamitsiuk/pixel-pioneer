a14340618a582354885ca81e0cb0c4d995e3da9f
Olha Mitsiuk <mitsyuk.olga@gmail.com>
2024-12-16 23:18:26 +0000

    fix product card

--- app/components/product-card/ProductCard.tsx
+++ app/components/product-card/ProductCard.tsx
@@ -1,25 +1,33 @@
 import Image from "next/image";
 
-const ProductCard = ({ title, imageSrc, description, price, tag }) => {
-  return (
-    <div className="card card-compact bg-base-100 w-72 shadow-xl">
-      <figure className="relative h-48">
-        <Image src={imageSrc} alt={title} layout="fill" objectFit="cover" />
-      </figure>
-      <div className="card-body flex flex-col">
-        <h3 className="card-title">{title}</h3>
-        <p className="text-gray-200">{description}</p>
-        <p className="font-semibold text-gray-200">${price}</p>
+interface ProductCardProps {
+	title: string;
+	imageSrc: string;
+	description: string;
+	price: number;
+	tag: string;
+}
 
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">View Product</button>
-        </div>
-      </div>
-      <div className="absolute top-1 left-1 z-10">
-        <span className="badge badge-secondary">{tag}</span>
-      </div>
-    </div>
-  );
+const ProductCard: React.FC<ProductCardProps> = ({ title, imageSrc, description, price, tag }) => {
+	return (
+		<div className="card card-compact bg-base-100 w-72 shadow-xl">
+			<figure className="relative h-48">
+				<Image src={imageSrc} alt={title} layout="fill" objectFit="cover" />
+			</figure>
+			<div className="card-body flex flex-col">
+				<h3 className="card-title">{title}</h3>
+				<p className="text-gray-200">{description}</p>
+				<p className="font-semibold text-gray-200">${price}</p>
+
+				<div className="card-actions justify-end">
+					<button className="btn btn-primary">View Product</button>
+				</div>
+			</div>
+			<div className="absolute top-1 left-1 z-10">
+				<span className="badge badge-secondary">{tag}</span>
+			</div>
+		</div>
+	);
 };
 
 export default ProductCard;
