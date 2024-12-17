0f257fee2084a51ad0db6c3390afe3129e75b737
Olha Mitsiuk <mitsyuk.olga@gmail.com>
2024-12-17 18:04:42 +0000

    fix deploy

--- app/basket/page.tsx
+++ app/basket/page.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useBasket } from '../context/BasketContext';
-import { BasketItem } from '../context/BasketContext';
 
 export default function BasketPage() {
 	const {
