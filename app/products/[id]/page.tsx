a9e678fb137c6607e243e073318f3cb0362762d7
Olha Mitsiuk <mitsyuk.olga@gmail.com>
2024-12-17 18:32:44 +0000

    fix deploy

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,29 +6,42 @@ import ProductDetails from "./ProductDetails";
 type Props = {
 	params: {
 		id: string;
-		[key: string]: string | string[];
 	};
-	searchParams: { [key: string]: string | string[] | undefined };
-}
+	searchParams?: { [key: string]: string | string[] | undefined };
+};
 
 export async function generateMetadata({
 	params
 }: Props): Promise<Metadata> {
-	const product = mockProducts.find(p => p.id === parseInt(params.id));
+	const id = Number(params.id);
+
+	if (isNaN(id)) {
+		return {
+			title: 'Product Not Found',
+		};
+	}
+
+	const product = mockProducts.find(p => p.id === id);
 
 	if (!product) {
 		return {
 			title: 'Product Not Found',
-		}
+		};
 	}
 
 	return {
 		title: product.product_name,
-	}
+	};
 }
 
 export default function Page(props: Props) {
-	const product = mockProducts.find(p => p.id === parseInt(props.params.id));
+	const id = Number(props.params.id);
+
+	if (isNaN(id)) {
+		return notFound();
+	}
+
+	const product = mockProducts.find(p => p.id === id);
 
 	if (!product) {
 		return notFound();
