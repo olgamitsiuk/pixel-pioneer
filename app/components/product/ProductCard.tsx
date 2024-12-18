29bd9c34b0aa1db395631ae6118c9f3f2fc9f400
Olha Mitsiuk <mitsyuk.olga@gmail.com>
2024-12-18 23:05:45 +0000

    fix product card

--- app/components/product/ProductCard.tsx
+++ app/components/product/ProductCard.tsx
@@ -2,38 +2,44 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface ProductCardProps {
-  title: string;
-  imageSrc: string;
-  description: string;
-  price: number;
-  id: number;
+	id: string;
+	title: string;
+	imageSrc: string;
+	description: string;
+	price: number;
+	tag: string;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({
-  title,
-  imageSrc,
-  description,
-  price,
-  id,
-}) => {
-  return (
-    <div className="card card-compact bg-base-100 w-72 shadow-xl">
-      <figure className="relative h-48">
-        <Image src={imageSrc} alt={title} layout="fill" objectFit="cover" />
-      </figure>
-      <div className="card-body flex flex-col">
-        <h3 className="card-title">{title}</h3>
-        <p className="text-gray-200">{description}</p>
-        <p className="font-semibold text-gray-200">£{price}</p>
+const ProductCard: React.FC<ProductCardProps> = ({ id, title, imageSrc, description, price, tag }) => {
+	return (
+		<div className="card card-compact bg-base-100 w-72 shadow-xl">
+			<figure className="relative h-48">
+				<Image
+					src={imageSrc}
+					alt={title || "Product image"}
+					fill
+					className="object-cover"
+					priority
+				/>
+			</figure>
+			<div className="card-body flex flex-col">
+				<h3 className="card-title">{title}</h3>
+				<p className="text-gray-200">{description}</p>
+				<p className="font-semibold text-gray-200">${Number(price).toFixed(2)}</p>
 
-        <div className="card-actions justify-end">
-          <Link href={`/products/${id}`}>
-            <button className="btn btn-primary">View Product</button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="card-actions justify-end">
+					<Link href={`/products/${id}`}>
+						<button className="btn btn-primary">View Product</button>
+					</Link>
+				</div>
+			</div>
+			{tag && (
+				<div className="absolute top-1 left-1 z-10">
+					<span className="badge badge-secondary">{tag}</span>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default ProductCard;
